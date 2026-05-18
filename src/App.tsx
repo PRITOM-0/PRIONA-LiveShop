d9b6b7016b60fc43bcc314064eeeab7c2afa8512
PRITOM-0 <pritomsahawww@gmail.com>
2026-05-19 01:06:40 +0600

    feat: update dependencies and add button component
    - Added new dependencies: @base-ui/react, @fontsource-variable/geist, class-variance-authority, clsx, lucide-react, shadcn, tailwind-merge, and tw-animate-css.
    - Updated @types/node to version 24.12.4.
    - Introduced a new Button component in src/components/ui/button.tsx with variants and styles.
    - Updated App component to include the new Button.
    - Enhanced index.css with custom themes and styles.
    - Configured TypeScript paths for easier imports.
    - Updated Vite configuration to resolve aliases for cleaner imports.
    - Created components.json for component configuration.
    - Added utility function for class name merging in src/lib/utils.ts.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 
 import './App.css'
-
+import { Button } from './components/ui/button'
 function App() {
   
   return (
     <>
       <div className='text-7xl text-center  mt-20'>Welcome to PRIONA</div>
+      <Button>Click me</Button>
     </>
   )
 }
